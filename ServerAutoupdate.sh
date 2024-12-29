6c632045486e2ed13c86b0add3458f173c08bce6
Anthony <anthonyflores5844@gmail.com>
2024-12-29 13:11:20 -0800

    Shellcheck fix quotes

--- ServerAutoupdate.sh
+++ ServerAutoupdate.sh
@@ -6,7 +6,8 @@ DEFAULT_DIR="/home/opc"
 while true; do
     # Get the current version of the modpack to know which directory to back up
     echo "Enter the current modpack version i.g. ($DEFAULT_DIR/Server-Files-2.11) is 2.11"
-    read currentVersion
+    # -r is needed, so backslashes are not treated as escape characters
+    read -r currentVersion
 
     # -d checks if specified path is a directory
     if [ -d "$DEFAULT_DIR/Server-Files-$currentVersion" ]; then
@@ -23,7 +24,7 @@ echo
 while true; do
     # Prompt the download link for the new server files
     echo "Enter the direct download link for the new server files:"
-    read url
+    read -r url
 
     # Grabs filetype and stores as variable
     linkFileType=$(basename "$url")
@@ -35,7 +36,7 @@ while true; do
         # The -P $DEFAULT_DIR specifies the destination directory ($DEFAULT_DIR) for the downloaded file.
         # The "$url" specifies the file to be downloaded from the given URL.
         # This ensures that the .zip file is saved directly into $DEFAULT_DIR, with its original filename.
-        wget -P $DEFAULT_DIR "$url"
+        wget -P "$DEFAULT_DIR" "$url"
 
         # stop the loop
         break
@@ -53,7 +54,7 @@ done
 echo "Creating backup for $DEFAULT_DIR/Server-Files-$currentVersion"
 # -a is archive, so it copies directoties and their permissions
 # -v is verbose to print out each file that is copied to terminal
-rsync -av --progress $DEFAULT_DIR/Server-Files-$currentVersion/ $DEFAULT_DIR/Backups/Server-Files-$currentVersion-Backup
+rsync -av --progress "$DEFAULT_DIR"/Server-Files-"$currentVersion"/ $DEFAULT_DIR/Backups/Server-Files-"$currentVersion"-Backup
 
 echo
 echo "Backup complete! Stored in $DEFAULT_DIR/Backups/Server-Files-$currentVersion-Backup/"
@@ -65,21 +66,21 @@ echo "Unzipping..."
 # The unzip command already makes a directory with the contents with the extraction, so just put it in ~
 # -q is quiet
 # -o overrides the prompt to confirm with sudo
-sudo unzip -qo $DEFAULT_DIR/Server-Files-$newVersion.zip -d $DEFAULT_DIR/
+sudo unzip -qo "$DEFAULT_DIR"/Server-Files-"$newVersion".zip -d "$DEFAULT_DIR"/
 # pv $DEFAULT_DIR/Server-Files-$newVersion.zip | unzip -q -d $DEFAULT_DIR/Server-Files-$newVersion/
 
 # Delete zip since folder now exists
-sudo rm -rf $DEFAULT_DIR/Server-Files-$newVersion.zip
+sudo rm -rf $DEFAULT_DIR/Server-Files-"$newVersion".zip
 
 # -r recursive
 # -u overwrites existing files undonditionally
 # /*: Ensures only the contents of the specified directories are copied, not the directories themselves.
-cp -ru $DEFAULT_DIR/Server-Files-$currentVersion/{world,local,server.properties,eula.txt,journeymap,user_jvm_args.txt,whitelist.json} $DEFAULT_DIR/Server-Files-$newVersion/
+cp -ru $DEFAULT_DIR/Server-Files-"$currentVersion"/{world,local,server.properties,eula.txt,journeymap,user_jvm_args.txt,whitelist.json} $DEFAULT_DIR/Server-Files-"$newVersion"/
 
 # Remove the old version folder after copy completed
-rm -rf $DEFAULT_DIR/Server-Files-$currentVersion
+rm -rf $DEFAULT_DIR/Server-Files-"$currentVersion"
 
 echo "DONE!"
 
 echo "Starting server right meow..."
-sudo bash $DEFAULT_DIR/Server-Files-$newVersion/startserver.sh
+sudo bash $DEFAULT_DIR/Server-Files-"$newVersion"/startserver.sh
